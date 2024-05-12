adf85971be9456a137e9acd062006d999b81db68
mortizp15 <manuelortizpelegrin@gmail.com>
2024-05-12 13:21:11 +0200

    favicon al layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Head from "next/head";
 import "./globals.css";
 import { montserrat } from "./ui/fonts";
 
@@ -11,6 +12,7 @@ export default function RootLayout({
   
   return (
     <html lang="en">
+      <link rel="icon" href="/assets/favicon.ico" sizes="any" />
       <body className={`${montserrat.className} absolute top-0 z-[-2] h-screen w-full bg-fixed bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]`}>
         {children}
       </body>
